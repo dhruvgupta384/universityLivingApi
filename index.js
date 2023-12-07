7101eaafd484d1a7980e796b047f92d548a9aa4e
Dhruvgupta1707 <dhruvgupta384@gmail.com>
2023-12-07 16:36:59 +0530

    added moongoose

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express =require('express')
 const cors=require('cors')
+const mongoose=require('mongoose')
 const app=express()
 app.use(cors());
-const apiData=require('./propertyData.json')
+// const apiData=require('./propertyData.json')
 
+mongoose.connect('mongodb+srv://dhruvGupta:1234@cluster0.ij2fd.mongodb.net/chapterkings').then((db)=>{
+    console.log("Connected")
+})
+.catch((err)=>{
+    console.error("Not Connected",err)
+})
+
+const Propertyschema=new mongoose.Schema({
+    unit:String,
+    rent:String,
+    deposit:String,
+    aminities:String,
+    i:String,
+    space:Object,
+    propertyName:String,
+    suiteName:String,
+    perWeekPrice:String
+}, { collection: 'propertyData' })
+const propData=mongoose.model('Property',Propertyschema)
 const port=process.env.PORT||3002
-app.get('/',(req,res)=>{
-    res.send(apiData)
+app.get('/',async (req,res)=>{
+    const data=await propData.find({});
+    // console.log(data)
+    res.send(data)
 })
 
 app.listen(port,()=>{
